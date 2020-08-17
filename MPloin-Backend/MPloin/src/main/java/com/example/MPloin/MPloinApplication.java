9426c39b7ee6103ed16abcfd0a7e411b9b7d08d3
gswain3316 <gswain3316@live.com>
2020-08-17 17:18:24 +0530

    Backend Initialized

---
+++ MPloin-Backend/MPloin/src/main/java/com/example/MPloin/MPloinApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPloin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPloinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MPloinApplication.class, args);
+	}
+
+}
