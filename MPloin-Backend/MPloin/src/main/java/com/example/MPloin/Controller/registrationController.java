da8385a749cbf0e03e8f01d03b8855b222257dff
gswain3316 <gswain3316@live.com>
2020-08-18 21:25:46 +0530

    regsiterController changes added for POST

--- MPloin-Backend/MPloin/src/main/java/com/example/MPloin/Controller/registrationController.java
+++ MPloin-Backend/MPloin/src/main/java/com/example/MPloin/Controller/registrationController.java
@@ -1,14 +1,25 @@
 package com.example.MPloin.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.MPloin.DataModel.Employee;
 
 @RestController
 public class registrationController {
 
-	@GetMapping("/register")
-	public String registerConfirmed()
+	@GetMapping("/test")
+	public String test()
 	{
+		return "Test Controller !!";
+	}
+	
+	@PostMapping("/register")
+	public String registerConfirmed(@RequestBody Employee empl)
+	{
+		
 		return "Registration is completed successfully !!";
 	}
 	
