077abbbe2e2a5aa621551033abc1448c4b9f27f5
gswain3316 <gswain3316@live.com>
2020-08-19 13:05:22 +0530

    employee repository & registration post method updated

--- MPloin-Backend/MPloin/src/main/java/com/example/MPloin/Controller/registrationController.java
+++ MPloin-Backend/MPloin/src/main/java/com/example/MPloin/Controller/registrationController.java
@@ -1,15 +1,25 @@
 package com.example.MPloin.Controller;
 
+import java.net.URI;
+
+import org.apache.catalina.authenticator.SavedRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.MPloin.DataModel.Employee;
+import com.example.MPloin.Repository.EmplRepository;
 
 @RestController
 public class registrationController {
 
+	@Autowired
+	private EmplRepository empRepo ;
+	
 	@GetMapping("/test")
 	public String test()
 	{
@@ -17,10 +27,13 @@ public class registrationController {
 	}
 	
 	@PostMapping("/register")
-	public String registerConfirmed(@RequestBody Employee empl)
+	public ResponseEntity<Object> registerConfirmed(@RequestBody Employee empl)
 	{
-		
-		return "Registration is completed successfully !!";
+		Employee emp = empRepo.save(empl);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{company}").
+				buildAndExpand(emp.getCompany()).toUri();
+//		return "Registration is completed successfully !!";
+		return ResponseEntity.created(location).build();
 	}
 	
 }
---
+++ MPloin-Backend/MPloin/src/main/java/com/example/MPloin/Repository/EmplRepository.java
@@ -0,0 +1,11 @@
+package com.example.MPloin.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.MPloin.DataModel.Employee;
+
+@Repository
+public interface EmplRepository extends CrudRepository<Employee, String> {
+		
+}
